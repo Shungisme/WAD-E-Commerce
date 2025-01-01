f730d33b7cc9d00a0f042a3c898bfcff30e35b51
22120118 <Shungisme@gmail.com>
2025-01-01 21:50:42 +0700

    Add API for chart

--- WAD-E-Commerce-product-backend/Product Management System/src/apis/v1/routes/order.router.js
+++ WAD-E-Commerce-product-backend/Product Management System/src/apis/v1/routes/order.router.js
@@ -6,7 +6,9 @@ import { isAdmin } from '../middlewares/role.middleware.js';
 const router = Router();
 
 router.get('/', OrderController.getAllOrders);
+router.get('/statistics', isAuthorized, isAdmin, OrderController.getStatistics);
 router.get('/:id', OrderController.getOrderById);
+
 router.post('/create', isAuthorized, OrderController.createOrder);
 router.patch('/update/:id', isAuthorized, OrderController.updateOrder);
 router.delete('/delete/:id', isAuthorized, OrderController.deleteOrder);
