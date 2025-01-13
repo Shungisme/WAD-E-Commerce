5ae907c87b3a213cf39447bd1abbbcf447de88a7
Zjn <129529039+zjn1404@users.noreply.github.com>
2025-01-13 17:59:27 +0700

    Update connect to hosted mysql

--- WAD-E-Commerce-product-backend/Payment-Mangement-System/src/configs/db.js
+++ WAD-E-Commerce-product-backend/Payment-Mangement-System/src/configs/db.js
@@ -2,14 +2,19 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const HOST = process.env.MYSQL_HOST;
+const PORT = process.env.MYSQL_PORT;
 const USER = process.env.MYSQL_USER;
 const PASSWORD = process.env.MYSQL_PASSWORD;
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
 
 const sequelize = new Sequelize(MYSQL_DATABASE, USER, PASSWORD, {
   host: HOST,
+  port: PORT,
   dialect: "mysql",
   timezone: "+07:00",
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 const mysqlConnection = async () => {
