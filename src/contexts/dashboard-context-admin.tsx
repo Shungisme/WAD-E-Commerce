46f68a7068a59a94a01214e9b7ed326abb7f17f3
22120415 <tranquangtuyen135305@gmail.com>
2025-01-13 16:28:09 +0700

    update dashboard admin

--- src/contexts/dashboard-context-admin.tsx
+++ src/contexts/dashboard-context-admin.tsx
@@ -3,6 +3,8 @@ import {
   Dispatch,
   ReactNode,
   SetStateAction,
+  useCallback,
+  useMemo,
   useState,
 } from "react";
 import { getStatisticsApi } from "../services/order";
@@ -14,28 +16,44 @@ interface Props {
 
 interface DashboardContextAdminType {
   getStatistics: UseQueryResult<any, unknown> | null;
+  getOldStatistics: UseQueryResult<any, unknown> | null;
+  previousYear: number | null;
   year: number | null;
   setYear: Dispatch<SetStateAction<number>>;
+  handleChangeYear: (value: number) => void;
+  compareOldYear: any | null;
 }
 
 const initial: DashboardContextAdminType = {
   getStatistics: null,
+  getOldStatistics: null,
+  previousYear: null,
   year: null,
   setYear: () => {},
+  handleChangeYear: () => {},
+  compareOldYear: null,
 };
 
 export const DashboardContextAdmin =
   createContext<DashboardContextAdminType>(initial);
 
 export default function DashboardProviderAdmin({ children }: Props) {
-  const [year, setYear] = useState<number>(new Date().getFullYear() - 2);
+  const [year, setYear] = useState<number>(new Date().getFullYear());
+  const [previousYear, setPreviousYear] = useState<number>(
+    new Date().getFullYear() - 1
+  );
+
+  const handleChangeYear = useCallback((value: number) => {
+    setYear(value);
+    setPreviousYear(value - 1);
+  }, []);
 
   const getStatistics = useQuery({
     queryKey: ["statistics", year],
     queryFn: async () => {
       try {
         const data = await getStatisticsApi(year);
-        console.log("statistics data", data);
+        console.log(data);
         return {
           year: data.yearlyTotalQuantityByCategory,
           month: data.monthlyTotalQuantityByCategory,
@@ -47,12 +65,49 @@ export default function DashboardProviderAdmin({ children }: Props) {
     },
   });
 
+  const getOldStatistics = useQuery({
+    queryKey: ["statistics", previousYear],
+    queryFn: async () => {
+      try {
+        const data = await getStatisticsApi(previousYear);
+
+        return {
+          year: data.yearlyTotalQuantityByCategory,
+          month: data.monthlyTotalQuantityByCategory,
+        };
+      } catch (error) {
+        console.log("error at getStatistics in services");
+        throw error;
+      }
+    },
+  });
+
+  const compareOldYear = useMemo(() => {
+    return {
+      categories: getStatistics?.data?.year.categories,
+      series: [
+        {
+          name: year,
+          data: getStatistics?.data?.year?.series,
+        },
+        {
+          name: previousYear,
+          data: getOldStatistics?.data?.year?.series,
+        },
+      ],
+    };
+  }, [getStatistics, getOldStatistics, year, previousYear]);
+
   return (
     <DashboardContextAdmin.Provider
       value={{
         getStatistics,
+        getOldStatistics,
+        previousYear,
         year,
         setYear,
+        handleChangeYear,
+        compareOldYear,
       }}
     >
       {children}
